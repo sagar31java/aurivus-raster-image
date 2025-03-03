67357582f3f24c2f3c124c210474d07c396a131d
sagar31java <sagardatta31@gmail.com>
2025-03-03 20:09:57 +0100

    finalImg

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import CellGrid from './components/CellGrid';
 import './css/raster.css';
 
 const App = () => {
-  const [rows] = useState(2);
-  const [cols] = useState(3);
+  const [rows] = useState(2); // setting initial value of rows of cells
+  const [cols] = useState(3); // setting initial value of columns of cells
 
   return (
     <div className="app">
       
       <div className="container">
-        <CellGrid rows={rows} cols={cols} />
+        <CellGrid rows={rows} cols={cols} /> {/*  CellGrid is a functional component which renders all the functionality of the application. */}
       </div>
     </div>
   );
--- src/components/CellGrid.jsx
+++ src/components/CellGrid.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
-import '../css/cell-grid.css';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import VisibilityIcon from '@mui/icons-material/Visibility';
+import '../css/cell-grid.css'; //css for managing css for CellGrid
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'; // using Material UI for eye cross icon
+import VisibilityIcon from '@mui/icons-material/Visibility'; // using Material UI for show eye icon
 
 
 export const CellGrid = ({ rows, cols }) => {
-  const [visibleCells, setVisibleCells] = useState(Array(rows * cols).fill(true));
+  const [visibleCells, setVisibleCells] = useState(Array(rows * cols).fill(true)); //creating hooks to store array of[rows,columns]
 
   const toggleCellVisibility = (index) => {
-    const newVisibility = [...visibleCells];
-    newVisibility[index] = !newVisibility[index];
+    const newVisibility = [...visibleCells];  // setting new visibility whenever eye icon on a cell is clicked
+    newVisibility[index] = !newVisibility[index]; 
     setVisibleCells(newVisibility);
   };
 
   return (
     <div className="grid-container">
-      <div className="cell-list">
-        {Array.from({ length: rows * cols }, (itr, index) => (
+      <div className="cell-list"> {/*list of cells, that will be displayed on left-side*/}
+        {Array.from({ length: rows * cols }, (itr, index) => ( //iterating array of cells with given rows and columns, and iterating by index to create display of each cell
           <div
             key={index}
             className="cell-item"
@@ -25,22 +25,22 @@ export const CellGrid = ({ rows, cols }) => {
             onClick={() => toggleCellVisibility(index)}
           >
 
-            <span className="eye-icon">{visibleCells[index] ? <VisibilityIcon></VisibilityIcon> : <VisibilityOffIcon></VisibilityOffIcon>}</span>
-            <span className='cell-list-text'>&nbsp;{`Cell ${index + 1}`}</span>
+            <span className="eye-icon">{visibleCells[index] ? <VisibilityIcon></VisibilityIcon> : <VisibilityOffIcon></VisibilityOffIcon>}</span> {/* Ternary operator for conditional rendering eye icon*/}
+            <span className='cell-list-text'>&nbsp;{`Cell ${index + 1}`}</span> {/*iterating cells using index */}
           </div>
         ))}
       </div>
-      <div className="image-container">
+      <div className="image-container"> {/*container for displayimg image (shown on right-side) */}
         <img src="./images/img_static.jpg" alt="Raster House" className="static-image" />
         <div className="grid-overlay">
-          {Array.from({ length: rows }, (itr, rowIndex) =>
-            Array.from({ length: cols }, (itr, colIndex) => {
+          {Array.from({ length: rows }, (itr, rowIndex) => // iterating array on rows
+            Array.from({ length: cols }, (itr, colIndex) => { //iterating array on columns
               const index = rowIndex * cols + colIndex;
               return (
                 <div
                   key={index}
                   id={`cell-${index}`}
-                  className={`grid-cell ${visibleCells[index] ? '' : 'hidden'}`}
+                  className={`grid-cell ${visibleCells[index] ? '' : 'hidden'}`} //ternary operator for show/hide visibility of cells on image
                   style={{
                     gridRow: rowIndex + 1,
                     gridColumn: colIndex + 1,
